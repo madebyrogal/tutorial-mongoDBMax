994cc6ac32baf5909e4727f722b48c0e910055ea
Tomek Rogalski <tomasz@madebyrogal.com>
2020-03-14 09:57:20 +0100

    ADD Validation schema

---
+++ validators/posts.js
@@ -0,0 +1,40 @@
+db.createCollection("posts", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["title", "text", "creator", "comments"],
+      properties: {
+        title: {
+          bsonType: "string",
+          description: "Must be a string and it is required"
+        },
+        text: {
+          bsonType: "string",
+          description: "Must be a string and it is required"
+        },
+        creator: {
+          bsonType: "objectId",
+          description: "Must be an objectId and it is required"
+        },
+        comments: {
+          bsonType: "array",
+          description: "Must be an array and it is required",
+          items: {
+            bsonType: "object",
+            required: ["text", "author"],
+            properties: {
+              text: {
+                bsonType: "string",
+                description: "Must be a string and it is required",
+              },
+              author: {
+                bsonType: "objectId",
+                description: "Must be an objectId and it is required",
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+});
